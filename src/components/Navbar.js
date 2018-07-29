f4838b5156e0c6ffceeea04047b906f9f55c6dff
Hyunwoo Kang <khwken@gmail.com>
2018-07-29 01:46:39 -1000

    Bootsrap the new community app

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,37 +2,57 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import logo from '../img/sw_logo.png'
+import '../css/index.sass'
 
 const Navbar = () => (
-  <nav className="navbar is-transparent">
+  <nav className="navbar is-transparent is-light">
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
           <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+            <img id="sw_logo" src={logo} alt="scholars_way_logo"/>
           </figure>
         </Link>
       </div>
       <div className="navbar-start">
-        <Link className="navbar-item" to="/about">
-          About
+        <div className="navbar-item has-dropdown is-hoverable">
+          <Link className="navbar-link" to="/">
+            Clinic
+          </Link>
+          <div className="navbar-dropdown is-boxed">
+            <Link className="navbar-item" to="/">
+              Practitioners
+            </Link>
+            <Link className="navbar-item" to="/">
+              Services
+            </Link>
+            <Link className="navbar-item" to="/">
+              Expertise
+            </Link>
+            <hr className="navbar-divider"></hr>
+            <Link className="navbar-item" to="/">
+              Fees
+            </Link>
+            <Link className="navbar-item is-active" to="/">
+              Appointment
+            </Link>
+          </div>
+        </div>
+        <Link className="navbar-item" to="/">
+          Class
         </Link>
-        <Link className="navbar-item" to="/products">
-          Products
+        <Link className="navbar-item" to="/">
+          Events
+        </Link>
+        <Link className="navbar-item" to="/">
+          Contact
         </Link>
       </div>
       <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
+        <Link className="navbar-item" to="/products">
+          Products
+        </Link>
       </div>
     </div>
   </nav>
