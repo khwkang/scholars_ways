3a62d3b88217e6b8b7e6f3695579bcdf43720de5
Hyunwoo Kang <khwken@gmail.com>
2018-08-01 03:50:09 -1000

    minor fix

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -6,7 +6,7 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
   const PageContent = contentComponent || Content
   
   return (
-    <section id="css_testing" className="section section--gradient">
+    <section id="" className="section section--gradient">
       <div className="container">
         <div className="columns">
           <div className="column is-10 is-offset-1">
