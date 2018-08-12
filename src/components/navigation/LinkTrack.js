efb9bf99bed389bcece4c02c43fb5fcdba2d0ffd
Raymond Luong <raymondluong@Raymonds-MacBook-Pro.local>
2018-08-11 18:37:44 -1000

    clean console logs

--- src/components/navigation/LinkTrack.js
+++ src/components/navigation/LinkTrack.js
@@ -4,8 +4,6 @@ import Link from "gatsby-link";
 export default props => {
   const { to, analytics, ...other } = props;
   const track = () => {
-    console.log("to", to);
-    console.log("refer", document.referrer);
     analytics
       ? analytics.page({ path: to, referrer: document.referrer, ...analytics })
       : null;
