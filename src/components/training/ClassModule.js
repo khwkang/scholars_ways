b21672f6f9a8a610dc24f6c6e5ee68b090b4bb70
Hyunwoo Kang <khwken@gmail.com>
2018-08-12 21:28:57 -1000

    update class page layout and styling

--- src/components/training/ClassModule.js
+++ src/components/training/ClassModule.js
@@ -1,17 +1,18 @@
 import React from 'react'
-import Link from 'gatsby-link'
+import LinkTrack from "../LinkTrack.js";
 
 export default class ClassModule extends React.Component {
   render() {
     const cls = this.props.classes
-    console.log("ssssss", cls)
     return (
-      <div className="cls_module column" style={{ backgroundImage: `url(${cls.image_path})`}}>
-         <div className="">
-        <h3 className="cls_title">
-          {cls.title}
-        </h3>                
-      </div>
+      <div className="cls_module_container">
+        <LinkTrack to={`/class/` + cls.link_url}>
+          <div className="cls_module" style={{ backgroundImage: `url(${cls.image_path})`}}>
+            <h3 className="cls_title">{cls.title}</h3>        
+            <div className="bg_overlay">
+            </div>
+          </div>
+        </LinkTrack>
       </div>
     )
   }
--- src/templates/class-page.js
+++ src/templates/class-page.js
@@ -5,15 +5,21 @@ import ClassModule from '../components/training/ClassModule.js'
 export const ClassPageTemplate = ({ title, data }) => {
   const classes = data.classes
   return (
-    <section id="" className="section section--gradient">
+    <section id="class_page" className="section section--gradient">
       <div className="container">
-        <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-          {title}
-        </h2>
-        <div className="section columns">
-          {classes.map(cls => (
-            <ClassModule key={cls.title} classes={cls}/>
-          ))}
+        <div className="columns">
+          <div className="column is-9 is-offset-1">
+            <div className="section">
+              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+              {title}
+              </h2>  
+                <div className="content columns">
+                  {classes.map(cls => (
+                    <ClassModule key={cls.title} classes={cls}/>
+                  ))}
+                </div>
+            </div>
+          </div>  
         </div>
       </div>
     </section>
@@ -49,6 +55,7 @@ export const ClassPageQuery = graphql`
         title
         classes {
           title
+          link_url
           image_path
           description
           schedule_day
