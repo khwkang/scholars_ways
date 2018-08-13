4d4153aebff8c36cb3557c974979a592ef0e8f18
Raymond Luong <raymondluong@Raymonds-MBP.lan>
2018-08-13 12:50:03 -1000

    Use Link as alias of LinkTrack

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -1,7 +1,7 @@
-import React from "react"
-import logo from "../../img/sw_logo.png"
-import animation from "./animation.js"
-import LinkTrack from "../LinkTrack.js"
+import React from 'react';
+import logo from '../../img/sw_logo.png';
+import animation from './animation.js';
+import Link from '../LinkTrack.js';
 
 export default class Navbar extends React.Component {
   render() {
@@ -10,56 +10,56 @@ export default class Navbar extends React.Component {
         className="custom_scroll menu column is-2-desktop is-3-tablet is-hidden-touch navbar is-fixed-top"
         id="side_nav"
       >
-        <LinkTrack
+        <Link
           to="/#home_hero"
           className="logo_wrapper"
           onClick={animation.spinLogo}
         >
           <img id="sw_logo" src={logo} alt="scholars_way_logo" />
-        </LinkTrack>
+        </Link>
         <hr className="nav_hr" />
         <p className="menu-label">Sholars Ways</p>
         <ul className="menu-list">
           <li>
-            <LinkTrack to="/about">About</LinkTrack>
+            <Link to="/about">About</Link>
           </li>
         </ul>
         <hr className="nav_hr" />
         <p className="menu-label">Clinic</p>
         <ul className="menu-list">
           <li>
-            <LinkTrack to="/clinic/practitioner">Practitioners</LinkTrack>
+            <Link to="/clinic/practitioner">Practitioners</Link>
           </li>
           <li>
-            <LinkTrack to="/clinic/expertise">Expertise</LinkTrack>
+            <Link to="/clinic/expertise">Expertise</Link>
           </li>
           <li>
-            <LinkTrack to="/clinic/appointment">Appointment</LinkTrack>
+            <Link to="/clinic/appointment">Appointment</Link>
           </li>
         </ul>
         <hr className="nav_hr" />
         <p className="menu-label">Training</p>
         <ul className="menu-list">
           <li>
-            <LinkTrack to="/class">Classes</LinkTrack>
+            <Link to="/class">Classes</Link>
           </li>
           <li>
-            <LinkTrack to="/class/schedule">Schedule</LinkTrack>
+            <Link to="/class/schedule">Schedule</Link>
           </li>
         </ul>
         <hr className="nav_hr" />
         <ul className="menu-list">
           <li>
-            <LinkTrack to="/library">Library</LinkTrack>
+            <Link to="/library">Library</Link>
           </li>
           <li>
-            <LinkTrack to="/blog">Blog</LinkTrack>
+            <Link to="/blog">Blog</Link>
           </li>
           <li>
-            <LinkTrack to="/contact">Join Us</LinkTrack>
+            <Link to="/contact">Join Us</Link>
           </li>
           <li>
-            <LinkTrack to="/products">Products</LinkTrack>
+            <Link to="/products">Products</Link>
           </li>
         </ul>
       </aside>
