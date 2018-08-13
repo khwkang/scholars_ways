9f39f53296736c5be15ba69d1bed63dd0f09f565
Hyunwoo Kang <khwken@gmail.com>
2018-08-12 23:58:06 -1000

    create contact page

---
+++ src/components/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default class Contact extends React.Component {
+  render() {
+    const data = this.props.data
+    return (
+      <div className="container">
+        <div>
+          {data.description}
+        </div>
+      </div>
+    )
+  }
+}
---
+++ src/templates/contact-page.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Contact from '../components/Contact.js'
+
+export const ContactPageTemplate = ({ title, data }) => {
+  return (
+    <section id="class_schedule_page" className="section section--gradient">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-9 is-offset-1">
+            <div className="section">
+              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+              {title}
+              </h2>  
+                <div className="content columns">                  
+                  <Contact key={data.title} data={data}/>
+                </div>
+            </div>
+          </div>  
+        </div>
+      </div>
+    </section>
+  )
+}
+
+ContactPageTemplate.propTypes = {
+  title: PropTypes.string.isRequired,
+  data: PropTypes.object
+}
+
+const ContactPage = ({ data }) => {
+  const { frontmatter } = data.markdownRemark
+  return (
+    <ContactPageTemplate
+      title={frontmatter.title}
+      data={frontmatter}
+    />
+  )
+}
+
+ContactPage.propTypes = {
+  data: PropTypes.object.isRequired,
+}
+
+export default ContactPage
+
+export const ContactPageQuery = graphql`
+  query ContactPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      frontmatter {
+        title
+      }
+    }
+  }
+`
