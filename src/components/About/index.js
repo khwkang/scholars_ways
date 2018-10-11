cd1cbb80214ea23182025de5e20b5ad63d939765
Hyunwoo Kang <khwken@gmail.com>
2018-10-10 22:07:18 -1000

    add group picture on about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,15 +1,39 @@
 import React from "react";
-import { graphql } from "gatsby";
+import { StaticQuery, graphql } from "gatsby";
 import { get } from "lodash";
 import { HTMLContent } from "../Content";
 import { Container, PageTitle } from "./index.styled";
+import { Image } from "../Image";
 
-export const About = ({ data, content }) => {
+export const About = props => (
+  <StaticQuery
+    query={graphql`
+      query {
+        group_pic: file(relativePath: { eq: "about_group.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 1400, quality: 90) {
+              ...GatsbyImageSharpFluid_withWebp_noBase64
+            }
+          }                                       
+        }
+      }
+    `}
+    render={render(props)}
+  />
+);
+
+const render = (props) => imageQueryData => {  
+  const data = props.data
+  const content = props.content
+  console.log("alskdhfauehf   ", imageQueryData)
   return (
     <Container>
       <PageTitle>{get(data, "title")}</PageTitle>
       <p>{get(data, "text")}</p>
       {content && <HTMLContent content={content} />}
+      <Image 
+        fluid={get(imageQueryData, "group_pic.childImageSharp.fluid")}
+      />
     </Container>
   );
 };
